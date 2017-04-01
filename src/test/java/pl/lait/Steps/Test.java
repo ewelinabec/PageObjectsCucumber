2f3b4b1417c484e24e4f0cdc5395fc6b5fcfff63
viktor <cooler713@wp.pl>
2017-04-01 12:32:59 +0200

    Dwa mega inteligentne komenty dodane

--- src/test/java/pl/lait/Steps/Test.java
+++ src/test/java/pl/lait/Steps/Test.java
@@ -4,9 +4,16 @@ import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-
+/**
+ * PAMIĘTAJCIE! Komentarz ma być rozsądny!
+ * @author Vittorio
+ *
+ */
 @RunWith(Cucumber.class)
 @CucumberOptions(strict = false, format = { "pretty", "html:target/cucumber", "json:target/cucumber.json"},monochrome = true)
 public class Test {
 
+/**
+ * Nieprędko, aczkolwiek konsekwetnie zmierzamy do przerwy o godzinie 13:00, co jest bardzo logiczne w tym pięknym dniu 	
+ */
 }
